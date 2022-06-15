f716fcaa57a0b20eeb21db93b47c8cdfcbaac6e2
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-06-15 17:58:06 +0000

    tristeza

---
+++ pw2b2a2/src/main/java/fernando.lima/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package fernando.lima.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("Mensagem", "Resultado");
+        return "/home/index";
+    }
+}
+
+
+
+
